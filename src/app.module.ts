da13101c5d3835f61fe3378b16b5c3b08594f1dc
lexico4real <lexico4real@gmail.com>
2022-03-31 20:58:21 +0100

    add medications module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DronesModule } from './drones/drones.module';
+import { MedicationsModule } from './medications/medications.module';
 
 @Module({
-  imports: [DronesModule],
+  imports: [DronesModule, MedicationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
