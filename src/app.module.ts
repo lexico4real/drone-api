da70099f9e32bb54b4ffd02eb28b160e7d881083
lexico4real <lexico4real@gmail.com>
2022-03-31 21:05:46 +0100

    add database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DronesModule } from './drones/drones.module';
 import { MedicationsModule } from './medications/medications.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [DronesModule, MedicationsModule],
+  imports: [
+    DronesModule,
+    MedicationsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'musala-drone-api',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
