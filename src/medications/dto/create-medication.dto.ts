1be102f49fc8331a33525f6508ad4a0d997a336e
lexico4real <lexico4real@gmail.com>
2022-04-07 16:05:30 +0100

    set rules for medication name, code and image url

--- src/medications/dto/create-medication.dto.ts
+++ src/medications/dto/create-medication.dto.ts
@@ -4,7 +4,10 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CreateMedicationDto {
   @ApiProperty()
   @IsNotEmpty()
-  @Matches(/[a-zA-Z0-9_-]{2,20}/)
+  @Matches(/^[a-zA-Z0-9.\-_]+$/, {
+    message:
+      'Name can only contain alphanumeric characters, hyphen, underscore and/or spaces',
+  })
   name: string;
 
   @ApiProperty()
@@ -13,9 +16,16 @@ export class CreateMedicationDto {
 
   @ApiProperty()
   @IsNotEmpty()
+  @Matches(/[A-Z]{4,}/, {
+    message:
+      'Code must be at least 4 characters long and contain only uppercase letters',
+  })
   code: string;
 
   @ApiProperty()
   @IsNotEmpty()
+  @Matches(/(https?:\/\/.*\.(?:png|jpg|gif))/i, {
+    message: 'Image URL is not valid',
+  })
   image: string;
 }
